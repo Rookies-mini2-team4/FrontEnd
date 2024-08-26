21a05fba011968ba25a6672f3a30bb9bbc5d7bf9
hsj1024 <tjeudeud@gmail.com>
2024-08-26 22:34:02 +0900

    UserHome.jsx edit

--- src/services/CommentService.js
+++ src/services/CommentService.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
-const REST_API_BASE_URL =  'http://localhost:8081/api'
+const REST_API_BASE_URL = "http://localhost:8081/api";
 
-const COMMENT_REST_API_URL = `${REST_API_BASE_URL}/main/comments`
+//const REST_API_BASE_URL = "http://localhost:8080/api";
+
+const COMMENT_REST_API_URL = `${REST_API_BASE_URL}/main/comments`;
 
 //export const getCommentById = (id) => axios.get(`${COMMENT_REST_API_URL}/${id}`);
 export const getCommentById = (id) => {
-    const token = localStorage.getItem("jwtToken");
-    return axios.get(`${COMMENT_REST_API_URL}/${id}`, {
+  const token = localStorage.getItem("jwtToken");
+  return axios.get(`${COMMENT_REST_API_URL}/${id}`, {
     headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
-      },
-    });
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
+    },
+  });
 };
-
 
 // export const createComment = (comment) => axios.post(`${COMMENT_REST_API_URL}`, comment, {
 //     headers: {
@@ -22,16 +23,14 @@ export const getCommentById = (id) => {
 //     }
 // });
 export const createComment = (comment) => {
-    const token = localStorage.getItem("jwtToken");
-    return axios.post(`${COMMENT_REST_API_URL}`, comment, {
+  const token = localStorage.getItem("jwtToken");
+  return axios.post(`${COMMENT_REST_API_URL}`, comment, {
     headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
-      },
-    });
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
+    },
+  });
 };
-
-
 
 // export const updateComment = (id, comment) => axios.put(`${COMMENT_REST_API_URL}/${id}`, comment, {
 //     headers: {
@@ -39,24 +38,22 @@ export const createComment = (comment) => {
 //     }
 // });
 export const updateComment = (id, comment) => {
-    const token = localStorage.getItem("jwtToken");
-    return axios.put(`${COMMENT_REST_API_URL}/${id}`, comment, {
+  const token = localStorage.getItem("jwtToken");
+  return axios.put(`${COMMENT_REST_API_URL}/${id}`, comment, {
     headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
-      },
-    });
-};      
-
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
+    },
+  });
+};
 
 //export const deleteComment = (id) => axios.delete(`${COMMENT_REST_API_URL}/${id}`);
 export const deleteComment = (id) => {
-    const token = localStorage.getItem("jwtToken");
-    return axios.delete(`${COMMENT_REST_API_URL}/${id}`, {
+  const token = localStorage.getItem("jwtToken");
+  return axios.delete(`${COMMENT_REST_API_URL}/${id}`, {
     headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
-      },
-    });
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
+    },
+  });
 };
-    
