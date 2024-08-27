a0f49a682dcf30dc4923e073630c54aaa84c3505
hsj1024 <tjeudeud@gmail.com>
2024-08-27 20:55:59 +0900

    edit mainservice.js

--- src/services/MainService.js
+++ src/services/MainService.js
@@ -18,17 +18,15 @@ export const getPostById = (id) => {
 };
 
 //export const createPost = (post) => axios.post(`${REST_API_URL}/write`, post);
-export const createPost = (post) => {
+export const createPost = (formData) => {
   const token = localStorage.getItem("jwtToken");
-  return axios.post(`${REST_API_URL}/write`, post, {
+  return axios.post(`${REST_API_BASE_URL}/main/write`, formData, {
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "multipart/form-data",
       Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 추가
     },
   });
 };
-
-
 
 // export const updatePost = (id, post) =>
 //   axios.put(`${REST_API_URL}/update/${id}`, post, {
